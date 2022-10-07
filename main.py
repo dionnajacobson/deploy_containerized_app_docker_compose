ed489e885fb80fd41f5ddd40d2697d3a89cbbc2f
EC2 Default User <ec2-user@ip-10-0-9-54.ec2.internal>
2022-10-07 02:38:05 +0000

    fix main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import requests
 
 #We generate a new FastAPI app in the Prod environment
 #https://fastapi.tiangolo.com/
-app = FastAPI(title='Face-Bokeh and Face-Emotion', root_path='./')#root_path='/Prod/')
+app = FastAPI(title='Face-Bokeh and Face-Emotion')
 
 #Call your get function for a health Check
 #to receive both (face-bokeh and face-emotion)
 @app.get("/", tags=["Health Check"])
 async def root():
     return {requests.get(url='http://face-bokeh:8001'), requests.get(url='http://face-emotion:8002')}
+
 
 
 
